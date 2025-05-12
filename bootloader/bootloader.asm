b60a815152c85b5740fc79c63462df98030063ed
Kross1de <awesome.hubygu@yandex.ru>
2025-05-12 21:26:41 +0600

    работаем ворк ворк

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,30 +1,45 @@
 org 0x7C00
 use16
 
-start:
-      mov si, hello
-      call print
-      mov si, cr
-      call print
-      jmp start
+macro print msg {
+      if ~ msg eq si
+            push si
+            mov si,msg
+            end if
+            call print_string
+            if ~ msg eq si
+                  pop si
+                  end if
+}
+
+mov si, hello
+call panicfunc
 
 zavis:
       jmp zavis
 
+panicfunc:
+      print panicPrefix
+      print si
+      xor ax, ax
+      int 16h
+      jmp far 0FFFFh:0
+
 ;как использовать:
 ; mov si,какойто кал или строка как тут 'hello'
-; call print
-print:
+; call print_string
+print_string:
       lodsb     ; загружает si в al (так как DF == 0) si++
       or al, al ; если al == 0
       jz .tyda ; уходем
       mov ah, byte 0Eh  ; устанавливаем INT10,E прерывание 
       mov bx, word 03h 
       int 10h   ; вызываем прерывание
-      jmp print
+      jmp print_string
 .tyda:
       ret
 
+panicPrefix:  db "BOOT PANIC: ",0
 hello:   db "privet zagryschik",0
 cr:   db 13h,0
 
