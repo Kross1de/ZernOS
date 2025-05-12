b6d4d78ba5ebe8226869f4ef7c8c93ecc44570fe
Kross1de <awesome.hubygu@yandex.ru>
2025-05-12 22:32:40 +0600

    фух наконецто закончил

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,6 +1,18 @@
 org 0x7C00
 use16
 
+lbaPacket equ 07E00h
+virtual at lbaPacket
+lbaPacket.size              : dw              ?
+lbaPacket.count             : dw              ?
+lbaPacket.offset            : dw              ?
+lbaPacket.segment           : dw              ? 
+lbaPacket.sector0           : dw              ?
+lbaPacket.sector1           : dw              ?
+lbaPacket.sector2           : dw              ?
+lbaPacket.sector3           : dw              ?
+end virtual
+
 macro print msg {
       if ~ msg eq si
             push si
@@ -12,20 +24,28 @@ macro print msg {
                   end if
 }
 
+; конец стека: 0x500
+; sp, bp: 0x600
+
 .start:
+      cli
+      cld
+      xor ax, ax
+      mov ss, ax 
+      mov es, ax
+      mov sp, 600h 
+      mov bp, 600h
+      push cs
+      pop ds
+
+.checkDisk:
       mov byte [bootdrv], dl
       cmp dl, byte 080h
       jl .notHDD
 .hddFND:
-;AH       41h = номер функции для проверки расширениях[8];
-;DL       индекс диска
-;BX       55AAh
       mov ah, byte 41h 
       mov bx, word 55AAh
       int 13h
-;CF       установить при отсутствии, отчистить при наличии
-;BX       AA55h
-;CX       bit1: доступ к устройству с использованием структуры пакета
       jc .notLBA
       cmp bx,0AA55h
       jne .notLBA
@@ -38,7 +58,31 @@ macro print msg {
       mov si, notHDD
       jmp panicfunc
 .LBAok:
-       
+       mov word [lbaPacket.size], 16
+       mov word [lbaPacket.count], 1 
+       mov word [lbaPacket.segment], 80h ;0x80:0x00 -> 0x800
+       mov word [lbaPacket.offset], 0 
+       mov word [lbaPacket.sector0], 0  
+       mov word [lbaPacket.sector1], 2 
+       mov word [lbaPacket.sector2], 0 
+       mov word [lbaPacket.sector3], 0
+       mov ah, 42h 
+       mov si, lbaPacket
+       int 13h 
+       jnc .readERR
+.readERR:
+      mov si, sectorReadError
+      jmp panicfunc
+.readSuccess:
+      mov bx, [magicBytes]
+      mov cx, word [800h] 
+      cmp cx, bx
+      jne .badMagic
+      mov ax, 802h
+      jmp ax
+.badMagic:
+      mov si, notValidMagic
+      jmp panicfunc
 
 zavis:
       jmp zavis
@@ -65,13 +109,17 @@ print_string:
       ret
 
 ; переменные
-bootdrv      db 0
+bootdrv:      db 0
+magicBytes:  db 0F4h, 1Ch
 
 ; сообщения ошибок
 panicPrefix:  db "BOOT PANIC: ",0
 lbaNotFound:  db "Hard drive doesnt support LBA packet struct",0 
-notHDD    db "Not a valid harddrive",0
+notHDD    db "Not a valid harddrive",0 
+sectorReadError db "Couldn't read sector from drive",0
+notValidMagic db "Sector doesnt start with the expected magic bytes",0
 
 ; пад и сигнатура для биоса
 pad:      db        510      -      ($      -      $$)      dup 0
 bootsig:      db 55h,0AAh
+
--- makefile
+++ makefile
@@ -1,4 +1,8 @@
 bootloader:
+	mkdir build
 	make -C bootloader all
 
 all: bootloader 
+
+clean:
+	rm -r build
