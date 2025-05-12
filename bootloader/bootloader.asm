10809a91e961d690c4033e4b623ccf4e01a1c29f
Kross1de <awesome.hubygu@yandex.ru>
2025-05-12 20:38:00 +0600

    нормальный принт + крутая какашка

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,18 +1,32 @@
+org 0x7C00
 use16
 
-mov ah, 0Eh
-mov bx, 03h
-mov al, 'h'
-int 10h
-mov al, 'e'
-int 10h
-mov al, 'l'
-int 10h
-int 10h
-mov al, 'o'
-int 10h
+start:
+      mov si, hello
+      call print
+      mov si, cr
+      call print
+      jmp start
+
 zavis:
       jmp zavis
+
+;как использовать:
+; mov si,какойто кал или строка как тут 'hello'
+; call print
+print:
+      lodsb     ; загружает si в al (так как DF == 0) si++
+      or al, al ; если al == 0
+      jz .tyda ; уходем
+      mov ah, byte 0Eh  ; устанавливаем INT10,E прерывание 
+      mov bx, word 03h 
+      int 10h   ; вызываем прерывание
+      jmp print
+.tyda:
+      ret
+
+hello:   db "privet zagryschik",0
+cr:   db 13h,0
 
 pad:      db        510      -      ($      -      $$)      dup 0
 bootsig:      db 55h,0AAh
