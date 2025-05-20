45fc96e7dc23dc7b413125bff551099806376a4f
Kross1de <awesome.hubygu@yandex.ru>
2025-05-20 15:42:01 +0600

    CPP
    переписка

---
+++ build.sh
@@ -0,0 +1,8 @@
+nasm -f elf32 src/kernel/boot.asm -o kasm.o #компилируем загрузчик
+gcc -fno-stack-protector -m32 -c src/kernel/kernel.cpp -o kmain.o #компилируем ядро
+ld -m elf_i386 -T linker.ld -o kernel kasm.o kmain.o #линкуем их
+rm kasm.o kmain.o #удаляем временные файлы
+mv kernel build/boot/kernel.bin #переносим наш слинкованый файл в папку с настройками grub
+grub-mkrescue -o build.iso build #создаем образ диска
+rm build/boot/kernel.bin #build/boot/kernel.img #удаляем файлы в папке с настройками grub
+qemu-system-i386 -cdrom build.iso #запускаем получившийся файл
---
+++ src/kernel/kernel.cpp
@@ -0,0 +1,11 @@
+extern "C" void kernel_main() {
+    const char* hello = "Zernishko OS v0.1";
+    volatile unsigned char* video_memory = (volatile unsigned char*) 0xb8000;
+    for (int i = 0; hello[i] != '\0'; ++i) {
+        video_memory[i * 2] = hello[i];
+        video_memory[i * 2 + 1] = 0x0F;  // Белый текст на черном фоне
+    }
+
+    // Бесконечный цикл, чтобы остановить выполнение ядра
+    while (1) {}
+}
